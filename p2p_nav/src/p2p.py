a4589e200d047cae1564ea73a9ac5a13fe77797e
Filip <58272568+FilipSagalara@users.noreply.github.com>
2024-05-25 16:27:40 +0200

    Beautify p2p.py

--- p2p_nav/src/p2p.py
+++ p2p_nav/src/p2p.py
@@ -1,50 +1,48 @@
-#! /usr/bin/env python2.7
-#! /usr/bin/env bash
+#!/usr/bin/env python2.7
+
+import rospy
+import roslib
+import actionlib
 from p2p_nav.msg import ScanAction, ScanGoal
 from std_srvs.srv import Empty
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
-import actionlib
 from geometry_msgs.msg import PointStamped
-from move_base_msgs.msg import MoveBaseAction
 from visualization_msgs.msg import Marker
-import rospy
-import roslib
 
 roslib.load_manifest("p2p_nav")
 
-#Class for p2p navigation using navigation stack
 
 class P2PNav:
     def __init__(self):
         rospy.loginfo("P2PNav is initializing")
         self.points = []
-        
-        
+
         rospy.init_node("Rviz_marker_manager")
         self.marker_pub = rospy.Publisher("/visualization_marker", Marker, queue_size=1)
         rospy.Subscriber("/clicked_point", PointStamped, self.clicked_callback)
         self.counter = 0
 
-        rospy.loginfo("move base client initialized....")
-        rospy.loginfo("360scanner client initalized....")
+        rospy.loginfo("Move base client initialized....")
+        rospy.loginfo("360scanner client initialized....")
 
     def clicked_callback(self, msg):
-        print("callback dziala")
+        rospy.loginfo("Callback activated")
         marker = Marker()
         marker.header.frame_id = "map"
         marker.header.stamp = msg.header.stamp
 
-        # set shape, Arrow: 0; Cube: 1 ; Sphere: 2 ; Cylinder: 3
-        marker.type = 2
-        marker.action = 0  # add marker
+        # Set shape, Arrow: 0; Cube: 1; Sphere: 2; Cylinder: 3
+        marker.type = Marker.SPHERE
+        marker.action = Marker.ADD
 
-        marker.id = self.counter  # namespace
+        marker.id = self.counter
         self.counter += 1
 
         # Set the scale of the marker
         marker.scale.x = 0.5
         marker.scale.y = 0.5
         marker.scale.z = 0.5
+
         # Set the color
         marker.color.r = 0.0
         marker.color.g = 1.0
@@ -52,7 +50,6 @@ class P2PNav:
         marker.color.a = 1.0
 
         # Set the pose of the marker
-
         marker.pose.position.x = msg.point.x
         marker.pose.position.y = msg.point.y
         marker.pose.position.z = msg.point.z
@@ -62,21 +59,17 @@ class P2PNav:
         marker.pose.orientation.w = 1
 
         self.marker_pub.publish(marker)
-        # print(self.counter)
         self.points.append(marker)
-        print(int(len(self.points)))
+        rospy.loginfo(f"Number of points: {len(self.points)}")
 
     def run(self):
-
-        print("P2P NAV")
-        rospy.loginfo("P2P_Nav")
-        while len(self.points) > 0:
-            print("Moving to point")
+        rospy.loginfo("P2P NAV is running")
+        while self.points:
+            rospy.loginfo("Moving to point")
             current_goal = self.points.pop(0)
             self.movebase_client(current_goal)
 
     def movebase_client(self, point):
-
         client = actionlib.SimpleActionClient("move_base", MoveBaseAction)
         client.wait_for_server()
 
@@ -98,3 +91,14 @@ class P2PNav:
     def get_goals(self):
         return self.points
 
+
+class RobotController:
+    def __init__(self):
+        self.point_to_point_navigation = P2PNav()
+
+
+if __name__ == "__main__":
+    robot_controller = RobotController()
+
+    if len(robot_controller.point_to_point_navigation.get_goals()) == 2:
+        robot_controller.point_to_point_navigation.run()
